7ef549459895280039ce13ef0fa7f57ff4a389d6
antoniovga1965-glitch <antoniovga1965@gmail.com>
2025-12-27 07:31:45 +0300

    Create adddeletenote.js

---
+++ adddeletenote.js
@@ -0,0 +1,38 @@
+export function unitinput() {
+    const unitinput =  document.getElementById("unitinput");
+    const addunit = document.getElementById("addunit");
+    const deleteunit = document.getElementById("deleteunit");
+    const unitresults = document.getElementById("unitresults");
+
+
+
+    function addunitfunc() {
+        const unitvalue = unitinput.value.trim();
+
+        if(!unitvalue){
+            alert("please enter unit that your doing");
+            return;
+        }
+        const li = document.createElement("li");
+        li.textContent = unitvalue;
+        unitresults.appendChild(li);
+        unitresults.style.display = "block"
+
+        unitinput.value="";
+    }
+
+    
+
+
+    function deleteunitfunc() {
+        if(unitresults.lastElementChild){
+
+            unitresults.lastElementChild.remove();
+        }
+    }
+    
+    addunit.addEventListener("click",addunitfunc);
+
+    deleteunit.addEventListener("click",deleteunitfunc);
+
+}
